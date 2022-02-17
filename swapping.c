61e5e4efa9c50e6da570f1fed460fc1f138b551e
BhavneetGithub <99886335+BhavneetGithub@users.noreply.github.com>
2022-02-17 19:00:00 +0530

    swapping of numbers
    the output displayed will be swapping of numbers

---
+++ swapping.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int a,c,b;
+  a=20;
+  b=10;
+  c=30;
+  printf("Swapping of Numbers\n")
+    a=c;
+    c=b;
+    b=a;
+  printf("the  value of a is %d",b);
+    printf("the value of b is %d",a);
+    
+    
+}
