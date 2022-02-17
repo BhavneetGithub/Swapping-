ecca4bc6b4d600f9293d84ed9553ef0528c3f8e7
BhavneetGithub <99886335+BhavneetGithub@users.noreply.github.com>
2022-02-17 19:17:01 +0530

    swapping of numbers
    This will produce output of Swapping of Numbers

--- swapping.c
+++ swapping.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
-int main()
-{
-  int a,c,b;
-  a=20;
-  b=10;
-  c=30;
-  printf("Swapping of Numbers\n")
-    a=c;
-    c=b;
-    b=a;
-  printf("the  value of a is %d",b);
-    printf("the value of b is %d",a);
-    
-    
+int main() {
+  double first, second, temp;
+  printf("Enter first number: ");
+  scanf("%lf", &first);
+  printf("Enter second number: ");
+  scanf("%lf", &second);
+
+  // value of first is assigned to temp
+  temp = first;
+
+  // value of second is assigned to first
+  first = second;
+
+  // value of temp (initial value of first) is assigned to second
+  second = temp;
+
+  // %.2lf displays number up to 2 decimal points
+  printf("\nAfter swapping, first number = %.2lf\n", first);
+  printf("After swapping, second number = %.2lf", second);
+  return 0;
 }
